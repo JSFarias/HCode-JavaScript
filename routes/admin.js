4185e3a581d6fb4f86332732c896a87eae28d9d1
Jhonatas <jhonatas.s.farias@gmail.com>
2019-12-18 07:34:57 -0300

    admin login

--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,38 @@
 var conn = require('./../inc/db')
 var express = require('express')
 var router = express.Router()
+var users = require('./../inc/users')
 
 router.get('/', function(req, res, next){
     res.render('admin/index');
 });
 
-router.get('/login', function(req, res, next){
+router.get('/login', function(req, res, next){    
 
-    if(!req.session.views) req.session.views = 0;
+    users.render(req, res, null);
 
-    console.log("|SESSION: ", req.session.views++);
+});
 
-    res.render('admin/login');
+router.post('/login', function(req, res, next){
+
+    if(!req.body.email)
+        users.render(req, res, "Insira email");
+    else if(!req.body.password)
+        users.render(req, res, "Insira senha");
+    else{
+
+        users.login(req.body.email, req.body.password).then(user=>{
+
+            req.session.user = user;
+            res.redirect("/admin");
+
+        }).catch(err=>{
+            users.render(req, res, err.message);
+
+        });
+
+    }
+
 });
 
 router.get('/contacts', function(req, res, next){
