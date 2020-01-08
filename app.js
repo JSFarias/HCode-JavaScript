a5e2ff8c24dedbfbdd41a3dbf0ce05c2a4b8a048
Jhonatas <jhonatas.s.farias@gmail.com>
2020-01-08 08:41:12 -0300

    socket.io

--- app.js
+++ app.js
@@ -10,14 +10,28 @@ var RedisStore = require('connect-redis')(session)
 var redisClient = redis.createClient()
 
 var formidable = require('formidable')
+var http = require('http');
+var socket = require('socket.io');
 var path = require('path');
-
-var indexRouter = require('./routes/index');
-var adminRouter = require('./routes/admin');
 
 var app = express();
 
+var http = http.Server(app);
+var io = socket(http);
+
+io.on('connection', function(socket){
+
+  console.log('novo usuário conectado ******');  
+
+});
+
+var indexRouter = require('./routes/index')(io);
+var adminRouter = require('./routes/admin')(io);
+
+
 app.use(function(req, res, next){
+
+  req.body = {};
 
   if(req.method === 'POST'){
 
@@ -62,7 +76,7 @@ app.use(
 );
 
 app.use(logger('dev'));
-app.use(express.json());
+//app.use(express.json());
 //app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -87,4 +101,8 @@ app.use(function(err, req, res, next) {
 
 });
 
-module.exports = app;
+http.listen(3000, function(){
+  console.log('servidor em execução ****')
+});
+
+
