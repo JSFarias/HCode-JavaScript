9354c0ad79fca59ac154a04e7c337a90769c9656
Jhonatas <jhonatas.s.farias@gmail.com>
2020-01-13 23:18:58 -0300

    form and bootstrap added

--- js/script.js
+++ js/script.js
@@ -2,11 +2,28 @@
 
 console.log(resposta);*/
 
-var a = 10;
-var b = 22;
+const a = 10;
+const b = 22;
 
 function somar(a, b){
 	return a + b;
 }
 
-console.log(somar(1, 5));
+//console.log(somar(1, 5));
+
+
+document.querySelector('#calcular').addEventListener('click', e=>{
+
+let valueA = parseInt(document.querySelector('#valorA').value);
+let valueB = parseInt(document.querySelector('#valorB').value);
+
+let result = somar(valueA, valueB);
+
+
+if(valueA && valueB)
+	document.querySelector('.resultado').innerHTML = result;
+else
+	alert("Fill all the fields")
+
+	console.log('oi',e);
+})
